3a0050371fd9eeac3f0935cf8cb89e7b4947f6d5
Daniel Brunner <none@none>
2013-06-30 14:48:18 +0200

    erste Grundfunktionalitäten; Picture-Dekode und LZS, sowie (ungetestet) DCL Decode

---
+++ scicorelib/SCI/Drawing/PictureRow.cs
@@ -0,0 +1,153 @@
+using System;
+using System.IO;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.Collections.Generic;
+
+namespace SCI.Drawing
+{
+	public class PictureRow
+	{
+		private Size		InternalSize = new Size();
+		private object		Object;
+
+		public Int16		Len;
+		public byte			NumOfLoops;
+		public byte			IsScalable;
+		public byte			ScaleRes;
+		public Int32		PalOffset;
+		public Color[]		Entries;
+
+		public List<Picture> PictureList = new List<Picture>();
+
+		public void FromFile(string filename)
+		{
+			FileStream fs = new FileStream(filename, System.IO.FileMode.Open);
+			FromStream(fs);
+			fs.Close();
+		}
+		public void FromStream(System.IO.Stream stream)
+		{
+			System.IO.BinaryReader br = new BinaryReader(stream);
+
+			Len = br.ReadInt16();
+			NumOfLoops = br.ReadByte();
+			IsScalable = br.ReadByte();
+			br.ReadByte();
+			ScaleRes = br.ReadByte();
+			PalOffset = br.ReadInt32();
+			InternalSize.Width = br.ReadInt16();
+			InternalSize.Height = br.ReadInt16();
+
+			for ( int entry = 0; entry < NumOfLoops; entry++ )
+			{
+				Picture pict = new Picture();
+				pict.HeaderFromStream(stream);
+
+				PictureList.Add(pict);
+			}
+
+			br.BaseStream.Position = PalOffset;
+
+			Entries = DecodeColorInformation(br);
+
+			br.BaseStream.Position += 6; // Offset für Bildbeginn
+
+			for ( int entry = 0; entry < NumOfLoops; entry++ )
+			{
+				PictureList[entry].ColorDataFromStream(stream);
+			}
+
+			foreach(Picture item in PictureList)
+			{
+				item.DecodeImage(Entries);
+			}
+
+		}
+
+		public void Save(string filename)
+		{
+			throw new NotImplementedException();
+		}
+		public void Save(System.IO.Stream stream)
+		{
+			throw new NotImplementedException();
+		}
+
+		public Color[] DecodeColorInformation(string filename)
+		{
+			FileStream fileStream = new FileStream(filename, FileMode.Open);
+			BinaryReader binaryReader = new BinaryReader(fileStream);
+
+			Color[] colors = DecodeColorInformation(binaryReader);
+
+			binaryReader.Close();
+			fileStream.Close();
+
+			return colors;
+		}
+		public Color[] DecodeColorInformation(BinaryReader binaryReader)
+		{
+			Color[] colorArray;
+			Int16 NumberOfColors = 256; // mal fest angenommen
+			Int16 ColorOffset = 0;
+			Int16 UsedUsed; // Is Color Used flag
+
+			binaryReader.BaseStream.Position += 25;
+
+			ColorOffset = binaryReader.ReadByte();
+			binaryReader.BaseStream.Position += 3;
+
+			NumberOfColors = binaryReader.ReadByte();
+			binaryReader.BaseStream.Position += 2;
+			UsedUsed = binaryReader.ReadByte();
+			binaryReader.BaseStream.Position += 4;
+
+			colorArray = new Color[256];
+			
+			for ( int counter = 0; counter < 256; counter++ )
+			{
+				colorArray[counter] = Color.Black;
+			}
+
+			for ( int counter = ColorOffset; counter < NumberOfColors + ColorOffset; counter++ )
+			{
+				if ( binaryReader.BaseStream.Position + 4 <= binaryReader.BaseStream.Length )
+				{
+					if ( UsedUsed > 0 )
+					{
+					}
+					else
+					{
+						//colorInfoArray[counter].Used = (binaryReader.ReadByte() > 0) ? true : false;
+						binaryReader.ReadByte();
+					} 
+					Int16 red = binaryReader.ReadByte();
+					Int16 green = binaryReader.ReadByte();
+					Int16 blue = binaryReader.ReadByte();
+					colorArray[counter] = Color.FromArgb(255, red, green, blue);
+				}
+			}
+			
+			return colorArray;
+		}
+
+		public Int32 Height
+		{
+			get { return InternalSize.Height; }
+		}
+		public Int32 Width
+		{
+			get { return InternalSize.Width; }
+		}
+		public object Tag
+		{
+			get { return Object; }
+			set { Object = value; }
+		}
+		public Size Size
+		{
+			get { return InternalSize; }
+		}
+	}
+}
---
+++ scicorelib/SCI/IO/Compression/lzs.cs
@@ -0,0 +1,103 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SCI.IO.Compression
+{
+	public class LZS
+	{
+		private Int32 GetReverseBits(Int32 numbits, ref byte[] value, ref UInt64 srcpos)
+		{
+			Int32 result = 0;
+
+			if ( numbits > 0 )
+			{
+				Int32 bytpos = (Int32)(srcpos / 8);
+				Int32 bitpos = (Int32)(srcpos % 8);
+
+				if ( bytpos + 2 < value.Length )
+				{
+					result = value[bytpos] << 16 | value[bytpos + 1] << 8 | value[bytpos + 2];
+				}
+				else if ( bytpos + 1 < value.Length )
+				{
+					result = value[bytpos] << 16 | value[bytpos + 1] << 8;
+				}
+				else
+				{
+					result = value[bytpos] << 16;
+				}
+
+				result = (Int32)((result >> (24 - numbits - bitpos)) & ((1L << numbits) - 1));
+
+				srcpos += (UInt64)numbits;
+			}
+			return result;
+		}
+		private Int32 GetLength(ref byte[] value, ref UInt64 srcpos)
+		{
+			Int32 bits;
+			Int32 length = 2;
+			do
+			{
+				bits = GetReverseBits(2, ref value, ref srcpos);
+				length += bits;
+			} while ( (bits == 3) && (length < 8) );
+
+			if ( length == 8 )
+			{
+				do
+				{
+					bits = GetReverseBits(4, ref value, ref srcpos);
+					length += bits;
+				} while ( bits == 15 );
+			}
+
+			return length;
+		}
+		public void Unpack(byte[] inbuf, ref byte[] outbuf)
+		{
+			UInt64 sourceBufferPosition = 0;
+			Int32 outBufferPosition = 0;
+			Int32 tag = 0;
+			Int32 offset = 0;
+
+			do
+			{
+				tag = GetReverseBits((Int32)1, ref inbuf, ref sourceBufferPosition);
+
+				if ( 0 == tag )
+				{
+					outbuf[outBufferPosition++] = (byte)GetReverseBits(8, ref inbuf, ref sourceBufferPosition);
+				}
+				else
+				{
+					if ( 1 == (tag = GetReverseBits(1, ref inbuf, ref sourceBufferPosition)) )
+					{
+						if ( 0 == (offset = GetReverseBits(7, ref inbuf, ref sourceBufferPosition)) )
+						{
+							break;
+						}
+					}
+					else
+					{
+						offset = GetReverseBits(11, ref inbuf, ref sourceBufferPosition);
+					}
+
+					Int32 len = GetLength(ref inbuf, ref sourceBufferPosition);
+					Int32 dic = outBufferPosition - offset;
+
+					if ( dic < 0 )
+					{
+						break;
+					}
+
+					while ( len-- > 0 )
+					{
+						outbuf[outBufferPosition++] = outbuf[dic++];
+					};
+				}
+			} while ( true );
+		}
+	}
+}
